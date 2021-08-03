25353ee365423f31256540c29f6c97662232bffb
Lucy Ferrabee <lucy.ferrabee@hotmail.com>
2021-08-03 14:15:59 +0100

    updated the routes to speak to the db

--- index.js
+++ index.js
@@ -1,19 +1,54 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const exphbs = require('express-handlebars');
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
+const app = express();
+app.use(express.json());
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
 const port = 3000
 
-app.get('/', (request, response) => {
-    response.send('hello this is the main to do list page');
+const url = "mongodb://root:password@localhost:27017"
+
+// get all tasks
+app.get('/tasks', async (request, response) => {
+    const connection = await MongoClient.connect(url)
+    const db = connection.db('todolistapp')
+    const collection = db.collection('tasks')
+    const data = await collection.find({}).toArray();
+    console.log(data);
+    response.json(data);
 })
 
-app.get('/add-an-item', (req, res) => {
-    res.send('this is where a new item is added')
+// create a new task
+app.post('/tasks', async (request, response) => {
+    const connection = await MongoClient.connect(url)
+    const db = connection.db('todolistapp')
+    const collection = db.collection('tasks')
+    console.log(request.body)
+    const result = await collection.insertOne(request.body);
+    response.json({"success": result.insertedId !== undefined})
 })
 
-app.get('/completed-items', (req, res) => {
-    res.send('this is where you see completed items')
-})
+// update a task to completed by id
+app.put('/tasks/:id', async (request, response) => {
+    const connection = await MongoClient.connect(url)
+    const db = connection.db('todolistapp')
+    const collection = db.collection('tasks')
+    const result = await collection.updateOne({_id: ObjectId(request.params.id)},{$set:{completed: true}});
+    console.log(result);
+    response.json(result);
+});
 
 app.listen(port, () => {
     console.log('Server running');
 })
+
+// just respond with a json.
+// communicate with it from postman. send it json and it will send you back json.
+// connect it to mongo to store tasks, so it should ask mongo for all the tasks that it has. then return with them with json.
+// it's up to you with how confident you are with the module system. get all three routes working on one file, then split up into modules.
